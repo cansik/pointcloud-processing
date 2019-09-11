1a2f09cf7ab9c94982ce2a57e8031673806151dc
Florian <florian@bildspur.ch>
2019-09-11 23:25:05 +0200

    added libraries as dependecies

--- src/test/java/ch/bildspur/pointcloud.test/BasicSketch.java
+++ src/test/java/ch/bildspur/pointcloud.test/BasicSketch.java
@@ -3,8 +3,10 @@ package ch.bildspur.pointcloud.test;
 import ch.bildspur.pointcloud.PointCloudRenderer;
 import ch.bildspur.pointcloud.attribute.FloatAttribute;
 import ch.bildspur.pointcloud.PointCloudBuffer;
+import peasy.PeasyCam;
 import processing.core.PApplet;
 import processing.core.PVector;
+import processing.opengl.PJOGL;
 import processing.opengl.PShader;
 
 
@@ -19,6 +21,8 @@ public class BasicSketch extends PApplet {
     PointCloudRenderer pclRenderer = new PointCloudRenderer(this);
     PointCloudBuffer pclBuffer;
 
+    PeasyCam cam;
+
     public void run()
     {
         runSketch();
@@ -28,36 +32,45 @@ public class BasicSketch extends PApplet {
     public void settings()
     {
         size(500, 500, P3D);
+        PJOGL.profile = 4;
     }
 
     @Override
     public void setup()
     {
+        cam = new PeasyCam(this, 400);
+
         // setup renderer
         shader = loadShader("shader/basicPointFrag.glsl", "shader/basicPointVertex.glsl");
         pclRenderer.setShader(shader);
 
         // setup pointcloud
-        pclBuffer = new PointCloudBuffer(100);
+        pclBuffer = new PointCloudBuffer(500);
         FloatAttribute positionAttribute = new FloatAttribute("position", 4);
 
         pclBuffer.addAttribute(positionAttribute);
-        pclBuffer.allocate();
 
-        pclRenderer.attach(pclBuffer);
+        pclBuffer.allocate();
 
         // fill with random points
         for(int i = 0; i < pclBuffer.getLength(); i++) {
             PVector v = PVector.random3D();
-            v.mult(100);
-
+            v.mult(10);
             positionAttribute.set(i * positionAttribute.getElementSize(), v.x, v.y, v.z);
         }
+
+        pclRenderer.attach(pclBuffer);
     }
 
     @Override
     public void draw() {
-        background(0);
+        background(100);
+
+        box(50);
+
+        g.push();
+        g.translate(0, 0, 0);
         pclRenderer.render(pclBuffer);
+        g.pop();
     }
 }
